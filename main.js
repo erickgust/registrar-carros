1315fd36bf1df0446f4103224ef7a1bcd511caf2
Erick Gustavo <erick.stazs@gmail.com>
2022-02-15 16:55:02 -0300

    Create createNewCar function

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
-import './style.css'
+const $form = getElement('cars-form');
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+function getElement(elementName) {
+  return document.querySelector(`[data-js="${elementName}"]`);
+}
+
+$form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const $tableCar = getElement('table-car');
+  $tableCar.appendChild(createNewCar());
+});
+
+function createNewCar() {
+  const $tr = document.createElement('tr');
+  const $tdImage = document.createElement('td');
+  const $tdModel = document.createElement('td');
+  const $tdYear = document.createElement('td');
+  const $tdPlate = document.createElement('td');
+  const $tdColor = document.createElement('td');
+
+  $tdImage.textContent = getElement('image').value;
+  $tdModel.textContent = getElement('model').value;
+  $tdYear.textContent = getElement('year').value;
+  $tdPlate.textContent = getElement('plate').value;
+  $tdColor.textContent = getElement('color').value;
+
+  $tr.appendChild($tdImage);
+  $tr.appendChild($tdModel);
+  $tr.appendChild($tdYear);
+  $tr.appendChild($tdPlate);
+  $tr.appendChild($tdColor);
+
+  return $tr;
+}
